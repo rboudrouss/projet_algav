7fdc5be809f221037eb4e998e156290bc5058797
rboudrouss <r.boudrouss@gmail.com>
2024-12-28 23:25:28 +0100

    update graph

--- docs/scripts/graph.py
+++ docs/scripts/graph.py
@@ -3,19 +3,19 @@ import matplotlib.pyplot as plt
 # Define the x values
 x = [5, 100, 500, 1000, 5000, 10000, 20000, 30000]
 
-y1 = [358, 14964, 49867, 86032, 291813, 465553, 731537, 1024370]
-plt.plot(x, y1, label='list words avec balance')
+y1 = [36, 157, 174, 184, 208, 230, 223, 228 ]
+plt.plot(x, y1, label='Hybrid trie')
 
 
-y2 = [298, 15960, 55378, 97819, 301890, 505042, 760740, 1150691]
-plt.plot(x, y2, label='list words sans balance')
+y2 = [58, 112, 134, 150, 181, 201, 286, 293]
+plt.plot(x, y2, label='Patricia trie')
 
 
 
 # Add labels and title
 plt.xlabel("taille de mot ajouté dans l'arbre")
 plt.ylabel('temps d\'execution en nano secondes')
-# plt.title('Graph of Two Y Indices')
+plt.title('Comparaison Search')
 
 # Add a legend
 plt.legend()
